3cdaef8a904697291163eccc925628d025e0fba1
raphaelrrl <raphaelrrl@gmail.com>
2023-02-08 20:00:17 -0300

    Update openvas-install.sh

--- openvas-install.sh
+++ openvas-install.sh
@@ -9,6 +9,15 @@
 	
 #iniciar serviço - '494d515d-d14b-4d84-abe0-4281fdb3d72d'
 	gvm-setup
+	sudo gvm-check-setup
+	
+	greenbone-feed-sync --type GVMD_DATA
+	greenbone-feed-sync --type SCAP
+	greenbone-feed-sync --type CERT
+	sudo -u _gvm greenbone-nvt-sync --rsync
+	sudo greenbone-scapdata-sync
+	sudo greenbone-certdata-sync
+	gvm-feed-update
 		
 #ajustar acesso via endereço IP externo
 	nano /usr/lib/systemd/system/greenbone-security-assistant.service
