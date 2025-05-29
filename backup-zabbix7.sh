2468cdd2f0cf6e683c9617f0b35a36a9b7d7f1da
raphaelrrl <raphaelrrl@gmail.com>
2025-05-28 21:14:46 -0300

    Update backup-zabbix7.sh

--- backup-zabbix7.sh
+++ backup-zabbix7.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-#backup frontend,alert scripts, external scripts e databases mysql zabbix
-# para importar o dump do banco de dado, executar este comando mysqldump -u username -p database_name > data-dump.sql
+#UPGRADE ZABBIX 6 to ZABBIX 7
+#backup frontend,alert scripts, external scripts e databases mysql/postgre zabbix server
+#Crie um periodo de homologaçao do upgrade
+#Tenha um documento de processo de migraçao com todos os comandos
+#Aprenda com a homologacao
+#Revisar os passos, revisar backup, revisar o banco de dados, revisar zabbix server.
+#leia as notas de lançamento e a documentaçao oficial
+#Valide as integraçoes, scripts e coletas
+#passos
+#abrir manutenção
+#Analise servicos zabbix server
+#backup das configuracoes e banco de dados
+#atualizacao
+#validacao dos arquivos de configuracao
+#upgrade banco de dados
+#validar servicos operando.
+#validar arquivo de configuracao zabbix server
+#
+#INICIO DO SCRIPT
 #set variaveis
 user="zabbix"
 password="password"
@@ -42,13 +59,15 @@ cp -R /usr/lib/zabbix/externalscripts/* $backup_path/backup-$date/externalscript
 cp -R /etc/zabbix/* $backup_path/backup-$date/fileconf
 
 # File Cofiguration my.cnf
-cp -R /etc/mysql/mariadb.conf.d/50-server.cnf $backup_path/backup-$date/fileconf/mysql
+cp -R /etc/mysql/my.cnf $backup_path/backup-$date/fileconf/mysql
 
 # File Cofiguration my.cnf
-cp -R /etc/mysql/* $backup_path/backup-$date/fileconf/mysql
+cp -R /etc/mysql/my.cnf.d/* $backup_path/backup-$date/fileconf/mysql
+cp -R /etc/mysql/conf.d/* $backup_path/backup-$date/fileconf/mysql/conf.d
+cp -R /etc/mysql/mariadb.conf.d/* $backup_path/backup-$date/fileconf/mysql/mariadb.conf.d
 
 # Add File to .tar
-tar -cvf $backup_path/backup-$date.tar
+tar -czvf $backup_path/backup-$date.tar
 
 # Add File to .tar.gz
-gzip $backup_path/backup-$date.tar
+#gzip $backup_path/backup-$date.tar
